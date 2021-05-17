d323b5bb4cd7f31e347f8ad5126e7c3697623e83
faruqfadh <Faruq@ruangguru.com>
2021-05-17 20:48:36 +0700

    init

---
+++ pubsub/go.mod
@@ -0,0 +1,16 @@
+module practice/pubsub
+
+go 1.16
+
+require (
+	cloud.google.com/go/pubsub v1.10.3 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
+	golang.org/x/net v0.0.0-20210510120150-4163338589ed // indirect
+	golang.org/x/oauth2 v0.0.0-20210514164344-f6687ab2804c // indirect
+	golang.org/x/sys v0.0.0-20210514084401-e8d321eab015 // indirect
+	golang.org/x/tools v0.1.1 // indirect
+	google.golang.org/api v0.46.0 // indirect
+	google.golang.org/genproto v0.0.0-20210513213006-bf773b8c8384 // indirect
+	google.golang.org/grpc v1.37.1 // indirect
+)
---
+++ pubsub/publisher/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"cloud.google.com/go/pubsub"
+)
+
+func Publish(projectID, topicID, msg string) error {
+	// projectID := "my-project-id"
+	// topicID := "my-topic"
+	// msg := "Hello World"
+	ctx := context.Background()
+	client, err := pubsub.NewClient(ctx, projectID)
+	if err != nil {
+		return fmt.Errorf("pubsub.NewClient: %v", err)
+	}
+	defer client.Close()
+
+	t := client.Topic(topicID)
+	result := t.Publish(ctx, &pubsub.Message{
+		Data: []byte(msg),
+	})
+	// Block until the result is returned and a server-generated
+	// ID is returned for the published message.
+	id, err := result.Get(ctx)
+	if err != nil {
+		return fmt.Errorf("Get: %v", err)
+	}
+	fmt.Printf("Published a message; msg ID: %v\n", id)
+	return nil
+}
+
+var (
+	topic = flag.String("topic", "", "Set topic")
+	proj  = flag.String("proj", "", "set project")
+)
+
+func init() {
+	flag.Parse()
+}
+
+func main() {
+	fmt.Printf("project: %s | topic: %s\n", *proj, *topic)
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Enter Msg: ")
+		// reads user input until \n by default
+		scanner.Scan()
+		// Holds the string that was scanned
+		text := scanner.Text()
+		if len(text) != 0 {
+			err := Publish(*proj, *topic, text)
+			if err != nil {
+				log.Fatalf("err: %v", err)
+			}
+			fmt.Println("published msg: ", text)
+		} else {
+			// exit if user entered an empty string
+			break
+		}
+
+	}
+	// handle error
+	if scanner.Err() != nil {
+		fmt.Println("Error: ", scanner.Err())
+	}
+
+}
